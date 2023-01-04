19622f85543a3e37c8c798d0af0932f45d77908c
dei <50441839+kthrdei@users.noreply.github.com>
2023-01-05 07:32:46 +0900

    update workflow

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,10 +1,10 @@
 import pathlib
-import toml
+import tomli
 
 import qmonus_plugin_builder
 
 
 def test_pyproject_version_and_init_version_are_the_same():
     path = pathlib.Path(__file__).joinpath('../../pyproject.toml').resolve()
-    pyproject = toml.load(path)
+    pyproject = tomli.loads(path.read_text())
     assert pyproject['tool']['poetry']['version'] == qmonus_plugin_builder.__version__
